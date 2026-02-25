215a1d5f8d5a2f5ab56b86f98092200764938144
mslthn <msulthon1909@gmail.com>
2026-02-25 14:48:49 +0700

    feat: create subquery to show choiced menu and its size, variant, and subtotal

---
+++ query.sql
@@ -0,0 +1,23 @@
+-- ambil nama produk, size, variant, dan final price
+SELECT
+    p.name AS product,
+    ps.size_name,
+    pv.variant_name,
+    (p.price + ps.add_price + pv.add_price) AS total_price
+FROM "products" p
+LEFT JOIN "product_size" ps ON p.id = ps.product_id
+LEFT JOIN "product_variant" pv ON p.id = pv.product_id
+WHERE p.id = 3;
+
+-- membuat subtotal dari barang yag dipilih, tambahkan kuantitas
+-- menggunkan subquery
+SELECT
+    p.name AS product,
+    p.quantity,
+    ps.size_name,
+    pv.variant_name,
+    ((p.price + ps.add_price + pv.add_price)*p.quantity) AS subtotal
+FROM "products" p
+LEFT JOIN "product_size" ps ON p.id = ps.product_id
+LEFT JOIN "product_variant" pv ON p.id = pv.product_id
+WHERE p.id = '4' and ps.size_name = 'Medium' and pv.variant_name = 'Ice';
